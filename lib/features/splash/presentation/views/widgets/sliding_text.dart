d3943de4a61d8fea38e86067c14645e721de447a
Gamal Azzam <gamalazzam05@gmail.com>
2025-09-27 03:56:35 +0300

    add sliding animation to splash view

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({super.key, required this.slidingAnimation});
+final Animation<Offset> slidingAnimation;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (_,_)=> SlideTransition(
+        position: slidingAnimation,
+        child: const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,18 +1,50 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animatedContainer;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animatedContainer = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero).animate(
+          CurvedAnimation(parent: animatedContainer, curve: Curves.easeOut),
+        );
+
+    animatedContainer.forward();
+  }
+
+  @override
+  void dispose() {
+    animatedContainer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-       crossAxisAlignment: CrossAxisAlignment.stretch,
-       children: [
-         Image.asset(AssetsData.logo),
-         const Text('Read Free Books',textAlign: TextAlign.center,)
-       ],
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
     );
   }
 }
