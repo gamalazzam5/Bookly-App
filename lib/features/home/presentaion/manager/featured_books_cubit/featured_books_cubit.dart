581a3984b3c84ff0d46b6c9aa8e29eb2e4d04474
Gamal Azzam <gamalazzam05@gmail.com>
2025-10-01 08:24:51 +0300

    create newest Books cubit

--- lib/features/home/presentaion/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentaion/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/models/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
